c4c8284352dcbfb383ecafbc901f7bb9d532451c
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-05-30 01:33:27 +0530

    Added serve-favicon module and favicon.ico. Updated package.json to remove warnings from 'npm install'.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+'use strict';
+
 const
-    http = require( 'http' ),
-    express = require( 'express' ),
-    app = express(),
-    morgan = require( 'morgan' );
+  http = require( 'http' ),
+  express = require( 'express' ),
+  serveFavicon = require( 'serve-favicon' ),
+  app = express(),
+  morgan = require( 'morgan' );
 
 morgan( 'dev' ); // Use express logger in 'dev' mode.
+
+app.use( express.static( __dirname + '/static' ));
+app.use( serveFavicon( __dirname + '/static/favicon.ico' ));
+app.use( morgan( 'dev' ));
 
 app.get( '/', function( req, res ) {
  	console.info( 'Serving request for ', req.method, req.url);
