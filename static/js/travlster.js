55c844d9b1bf15e29197e52e6281c79f79b95cfb
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-06-23 02:05:44 +0530

    Added 3 templates. Refactored 2 previous ones.

--- static/js/travlster.js
+++ static/js/travlster.js
@@ -2,6 +2,16 @@ define( function() {
   var Travlster = {};
   Travlster.init = function() {
     console.log( 'travlster.js loaded.' );
+    Travlster.emberInit();
   };
+  Travlster.emberInit = function() {
+    console.log( 'emberInit' );
+    EmberApp = Ember.Application.create();
+    EmberApp.Router.map( function() {
+      this.resource( 'home' );
+      this.resource( 'about' );
+      this.resource( 'contact-us' );
+    });
+  }
   return Travlster;
 });
