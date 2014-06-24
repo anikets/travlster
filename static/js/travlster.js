d2e210572a3dd434c07c15f1dc9ecc6d9edb3f69
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2014-06-24 09:00:55 +0530

    Stub JSONs for bus stops and railway stations.

--- static/js/travlster.js
+++ static/js/travlster.js
@@ -18,3 +18,57 @@ define( function() {
   }
   return Travlster;
 });
+
+var TravlsterModel = {};
+
+TravlsterModel.busStops = [
+  {
+    "name": "Bus Stop 1",
+    "coords": "",
+    "distanceFromCurrentPosition": "100",  // Distance in meter.
+    "waypoints": [
+      {"name": "Origin", "coords": ""},
+      {"name": "Waypoint 1", "coords": ""},
+      {"name": "Waypoint 2", "coords": ""},
+      {"name": "Waypoint 3", "coords": ""},
+      {"name": "Destination", "coords": ""}],
+    "route": ""  // Google directions array.
+  },
+  {
+    "name": "Bus Stop 2",
+    "coords": "",
+    "distanceFromCurrentPosition": "200",  // Distance in meter.
+    "waypoints": [
+      {"name": "Origin", "coords": ""},
+      {"name": "Waypoint 1", "coords": ""},
+      {"name": "Waypoint 2", "coords": ""},
+      {"name": "Destination", "coords": ""}],
+    "route": ""  // Google directions array.
+  }
+];
+
+TravlsterModel.rlyStations = [
+  {
+    "name": "Railway Station 1",
+    "coords": "",
+    "distanceFromCurrentPosition": "1000",  // Distance in meter.
+    "waypoints": [
+      {"name": "Origin", "coords": ""},
+      {"name": "Waypoint 1", "coords": ""},
+      {"name": "Waypoint 2", "coords": ""},
+      {"name": "Waypoint 3", "coords": ""},
+      {"name": "Destination", "coords": ""}],
+    "route": ""  // Google directions array.
+  },
+  {
+    "name": "Railway Station 2",
+    "coords": "",
+    "distanceFromCurrentPosition": "1200",  // Distance in meter.
+    "waypoints": [
+      {"name": "Origin", "coords": ""},
+      {"name": "Waypoint 1", "coords": ""},
+      {"name": "Waypoint 2", "coords": ""},
+      {"name": "Destination", "coords": ""}],
+    "route": ""  // Google directions array.
+  }
+];
